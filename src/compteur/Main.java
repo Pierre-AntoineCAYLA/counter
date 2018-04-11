85901f4778afc759f5d79c0b57e9f6d682c79c74
Pierre-Antoine <you@example.com>
2018-04-11 14:26:08 +0200

    organisation

--- src/compteur/Main.java
+++ src/compteur/Main.java
@@ -3,45 +3,40 @@ package compteur;
 public class Main {
 
 	public static void main(String[] args) {
-int j=30;
 
+		//premier conteur
+		int j = 30;
 		Counter counter = new Counter();
 		System.out.println(counter.getValue());
-		for(int i=0;i< j; i++)
-		{
-		counter.inc();
-		System.out.println(counter.getValue());
-		}		
+		for (int i = 0; i < j; i++) {
+			counter.inc();
+			System.out.println(counter.getValue());
+		}
 		System.out.println(counter);
-		
-		
-int k=30;		
-		
+
+		//deuxieme compteur
+		int k = 30;
 		Counter counter2 = new Counter();
 		System.out.println(counter2.getValue());
-		for(int i=0;i< k; i++)
-		{
-		counter2.inc();
-		System.out.println(counter2.getValue());
-		}		
+		for (int i = 0; i < k; i++) {
+			counter2.inc();
+			System.out.println(counter2.getValue());
+		}
 		System.out.println(counter2);
-		
-int l=30;		
-		
+
+		//troisieme compteur
+		int l = 30;
 		Counter counter3 = new Counter();
 		System.out.println(counter3.getValue());
-		for(int i=0;i< l; i++)
-		{
-		counter3.inc();
-		System.out.println(counter3.getValue());
-		}		
+		for (int i = 0; i < l; i++) {
+			counter3.inc();
+			System.out.println(counter3.getValue());
+		}
 		System.out.println(counter3);
-		
+
+		//affichage
 		System.out.println(counter.equals3(counter2, counter3));
-
-		System.out.println(counter.getglobalCounter());
+		System.out.println(Counter.getglobalCounter());
 	}
-
-
 
 }
