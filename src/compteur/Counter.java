8e5c0cb47f66fd7ab2757d01a5fb8fa6a75a8b36
Pierre-Antoine <you@example.com>
2018-04-11 11:04:20 +0200

    classe créé

--- src/compteur/Counter.java
+++ src/compteur/Counter.java
@@ -1,5 +1,10 @@
 package compteur;
 
-public class Counter {
+public class Counter(int position, int step) {
+
+int compte=position;
+for(int i=0; i<step, i++)
+{compte=compte+1;}
+	return (compte);
 
 }
